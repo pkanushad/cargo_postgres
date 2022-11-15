6069393a171b6ddc1b6303b11384605c97e17127
pkanushad <pkanushad57@gmail.com>
2022-11-15 15:46:02 +0530

    modified host debug

--- cargo/cargo/settings.py
+++ cargo/cargo/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f)4)k&mdq5%g*f4_3hot*yk_n29-wz+)6=_^bb&3d-2nhqwl*&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','postgrecarg.herokuapp.com']
 
 
 # Application definition
